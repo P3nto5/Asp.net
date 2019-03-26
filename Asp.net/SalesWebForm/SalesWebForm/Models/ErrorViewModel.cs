295e7c37df86d13e7cf821649d08ae4cd5b91b19
Samuel viana da silva <sam226gold@gmail.com>
2019-03-26 15:47:14 -0400

    Add files via upload

---
+++ Asp.net/SalesWebForm/SalesWebForm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
